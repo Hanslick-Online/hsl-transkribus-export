3d3a713f5c2e77073d5a2b07a9b402071cc6b3e4
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2024-09-23 13:26:28 +0200

    adding second col

--- scripts/fetch_mets.py
+++ scripts/fetch_mets.py
@@ -12,3 +12,13 @@ transkribus_client = ACDHTranskribusUtils(
 )
 
 mpr_docs = transkribus_client.collection_to_mets(col_id, file_path='./mets')
+
+col_id = os.environ.get('COL_ID2')
+
+transkribus_client = ACDHTranskribusUtils(
+    user=user,
+    password=pw,
+    transkribus_base_url="https://transkribus.eu/TrpServer/rest"
+)
+
+mpr_docs = transkribus_client.collection_to_mets(col_id, file_path='./mets')
